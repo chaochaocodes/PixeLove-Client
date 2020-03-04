edb49cfcc0c1fba6fa9cd9e6afa5b73bbab13d60
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-03-04 11:39:06 -0800

    Added a simple button on Landing Page with some base styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,25 +1,28 @@
 // React Imports
-import React from 'react';
+import React from "react";
 
 // Custom Imports
-import Navbar from '../components/Navbar'
+import Navbar from "../components/Navbar";
 
 // This functional component renders the landing page
 const LandingPage = () => {
-    return (
-        <div className="bg-green-900 h-screen">
-            <Navbar />
-            <div className="bg-red-900 h-32">
-                <h1>PLAY IT FORWARD</h1>
-            </div>
-            <div className="bg-blue-900 h-24">
-                <h1>MAKE A DIFFERENCE</h1>
-            </div>
-            <div className="bg-yellow-900 h-">
-                <h1>EVENTS</h1>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar />
+      <div className="flex items-center justify-center bg-red-900 h-40">
+        <h1 className="font-bold">PLAY IT FORWARD</h1>
+      </div>
+      <div className="flex items-center justify-center bg-blue-900 h-16">
+        <button class="bg-blue-500 focus:outline-none hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+          Login
+        </button>
+      </div>
+      <div className="flex items-center justify-center bg-yellow-900 flex-grow">
+        <h1 className="font-bold">VIEW UPCOMING EVENTS</h1>
+        
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
