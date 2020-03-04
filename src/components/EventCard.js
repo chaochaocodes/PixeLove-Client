16dcbc48f2d1189a4152524d55dba796bda356b2
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-03-04 14:54:05 -0800

    Created a very basic Event Card with dummy data displaying

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,7 +2,7 @@
 import React from "react";
 
 // Custom Imports
-import pifLogo from '../media/icons/pif-logo.png'
+import pifLogo from "../media/icons/pif-logo.png";
 
 const EventCard = () => {
   const propSample = {
@@ -12,6 +12,7 @@ const EventCard = () => {
           id: 1,
           title: "Volunteering Event Title",
           date: "2020-03-28",
+          address: "1234 Somewhere St, Seattle, WA, 98177",
           nonprofit: {
             displayName: "Play It Forward",
             logo: "Insert url here"
@@ -21,17 +22,18 @@ const EventCard = () => {
     }
   };
 
-  console.log(propSample);
-
   return (
-    <div className="flex flex-col">
-    <div className="h-16 w-16">
-    <img className="object-cover" src={pifLogo} alt="" />
+    <div className="flex flex-col items-center bg-white w-48 rounded-lg overflow-hidden">
+      <div className="h-16 w-16">
+        <img className="object-cover" src={pifLogo} alt="" />
+      </div>
+      <div className="flex flex-col pl-2 bg-blue-400 w-full">
+        <h1 className="text-s">{propSample.data.event[0].nonprofit.displayName}</h1>
+        <h1 className="text-xl font-semibold">{propSample.data.event[0].title}</h1>
+        <h1 className="text-s">{propSample.data.event[0].date}</h1>
+        <h1 className="text-xs truncate">{propSample.data.event[0].address}</h1>
+      </div>
     </div>
-    <div>{propSample.data.event[0].title}</div>
-    <div>{propSample.data.event[0].date}</div>
-    <div>{propSample.data.event[0].nonprofit.displayName}</div>
-  </div>
   );
 };
 
