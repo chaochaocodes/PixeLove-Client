466633de1c533ec12416bbbf501caf7ee88301b5
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-05-09 12:03:09 -0700

    Update new room fetch

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,16 @@
-export const API_ROOT = "https://pixelove-server-app.herokuapp.com"
-export const WS_ROOT = 'wss://pixelove-server-app.herokuapp.com/cable'
+export const API_ROOT = "https://pixelove-server-app.herokuapp.com";
+export const WS_ROOT = "wss://pixelove-server-app.herokuapp.com/cable";
 const headers = () => {
-    return {
-      "Content-Type": "application/json",
-      Accepts: "application/json"
-    }; 
+  return {
+    "Content-Type": "application/json",
+    Accepts: "application/json"
   };
+};
 
-const newRoom = (params) => {
-
-
-}
+const newRoom = params => {
+  return fetch(`${API_ROOT}/login`, {
+    method: "POST",
+    headers: headers(),
+    body: JSON.stringify(data.room)
+  }).then(roomData => roomData.json());
+};
