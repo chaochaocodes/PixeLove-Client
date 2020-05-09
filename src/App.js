3e8b8ec50321da5611ad8c596fe8fa97ed3db2de
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-05-09 11:10:00 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
-// Custom Imports
-import Landing from './components/Landing';
-import Lobby from './components/Lobby';
-import Draw from './components/Draw';
-
-function App() {
-   return (
-      <div className="App">
-      <Router>
-         <Switch>
-            <Route exact path="/"><Landing/></Route>
-            <Route path="/lobby"><Lobby/></Route>
-            <Route path="/draw"><Draw/></Route>
-         </Switch>
-      </Router>
-      </div>
-   );
-=======
 import logo from './logo.svg';
 import './App.css';
 
@@ -42,7 +21,6 @@ function App() {
       </header>
     </div>
   );
->>>>>>> 8c951b09344c382bf2c91a56a07262b73e68a73f
 }
 
 export default App;
